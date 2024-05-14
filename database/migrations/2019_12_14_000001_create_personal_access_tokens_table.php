e77afb9c496307b008b0d8d1e7e01ab2bb2ec078
khoaminhle65 <khoaminhle65@gmail.com>
2024-05-15 00:26:17 +0700

    update

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,13 +13,17 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->string('tokenable_type', 191); // Adjust the length as needed
+            $table->unsignedBigInteger('tokenable_id');
             $table->string('name');
-            $table->string('token', 64)->unique();
+            $table->string('token', 100)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+            
+            // Add the index after creating the columns
+            $table->index(['tokenable_type', 'tokenable_id']);
         });
     }
 
